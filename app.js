1585c230ce9cb5bc0fba9d6489e79dda98858c43
Patrick John Dy Sagrado <dysagradop7199@gmail.com>
2025-06-07 20:53:56 +0800

    minor update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const nextButton = document.querySelector('.btn-conteiner');
 const video = document.querySelector('.hero-video');
 
-const movieList = ['videos/background1.mp4', 'videos/background2.mp4', 'videos/background3.mp4', 'videos/background4.mp4', 'videos/background5.mp4'];
+const movieList = ['videos/background1.mp4', 'videos/background2.mp4', 'videos/background3.mp4', 'videos/background5.mp4'];
 
 let index = 0;
 nextButton.addEventListener('click', function(){
@@ -9,7 +9,7 @@ nextButton.addEventListener('click', function(){
     index+=1
     video.src = movieList[index]
 
-    if (index === 4){
+    if (index === 3){
         index = -1;
     }
 })
