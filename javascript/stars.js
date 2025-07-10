32a9c15c337f60731102d4b64dd543f0c2fb05a5
Patrick John Dy Sagrado <dysagradop7199@gmail.com>
2025-07-10 22:44:04 +0800

    add

--- javascript/stars.js
+++ javascript/stars.js
@@ -1,64 +1,94 @@
 // Multiple leaderboard datasets
 const leaderboards = [
     {
-        title: "OFW ELITES 1 CWL LEADERBOARD",
-        updated: "UPDATED ON 5/28/25 : 3:20PM",
+        title: "*ASHANTY FAM * JULY 2025 CWL",
+        updated: "PROMOTED : CHAMPION LEAGUE I",
         players: [
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 }
+            { name: "Sawamura", stars: 21, percentage: 700 },
+            { name: "Rai Ley メ ❘ イノブ", stars: 21, percentage: 700 },
+            { name: "Gobta.", stars: 21, percentage: 700 },
+            { name: "OE☆☆☆ReNaN☆☆☆", stars: 21, percentage: 700 },
+            { name: "™Aliäs{{Pögi}}™", stars: 20, percentage: 696 },
+            { name: "K♥️L♥️H♥️A♥️Y", stars: 20, percentage: 692 },
+            { name: "Makario", stars: 19, percentage: 696 },
+            { name: "entengzki", stars: 19, percentage: 691 },
+            { name: "DON EX MACHINA", stars: 19, percentage: 678 },
+            { name: "❤❤SHALTEAR❤❤", stars: 18, percentage: 684 },
         ]
     },
     {
-        title: "OFW ELITES 2 CWL LEADERBOARD",
-        updated: "UPDATED ON 5/30/25 : 4:51PM",
+        title: "OFW ELITES 2 JULY 2025 CWL",
+        updated: "DEMOTED : CHAMPION LEAGUE II (LACK OF DATA)",
         players: [
-            { name: "PATRICK XD JDS", stars: 15, percentage: 87.5 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 }
+            { name: "TW™ seaweeds", stars: 21, percentage: 700 },
+            { name: "LostChild", stars: 21, percentage: 700 },
+            { name: "ЯЄD Ｎ⭕ＴＩＣＥ ᵘⁿ", stars: 20, percentage: 698 },
+            { name: "ChaMi", stars: 20, percentage: 696 },
+            { name: "✨⭐ YzeQuel⭐✨", stars: 20, percentage: 683 },
+            { name: "™Aliäs{{2015}}™", stars: 19, percentage: 688 },
+            { name: "NO DATA", stars: 19, percentage: 680 },
+            { name: "Aether..23", stars: 19, percentage: 675 },
+            { name: "mOSTSTER KILL", stars: 19, percentage: 672 },
+            { name: "PHT Bogs", stars: 18, percentage: 687 },
         ]
     },
     {
-        title: "OFW ELITES 3 CWL LEADERBOARD",
-        updated: "UPDATED ON 6/1/25 : 1:15PM",
+        title: "OFW ELITES 3 JULY 2025 CWL",
+        updated: "DEMOTED : CHAMPION LEAGUE III (LACK OF DATA)",
         players: [
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 }
+            { name: "KARMA BLITZ", stars: 21, percentage: 700 },
+            { name: "VHINZE", stars: 20, percentage: 699 },
+            { name: "MASTER KYASUBi", stars: 20, percentage: 694 },
+            { name: "K♦️L♦️H♦️A♦️Y", stars: 20, percentage: 693 },
+            { name: "LV. 88 ❌⭕❌⭕", stars: 19, percentage: 689 },
         ]
     },
     {
-        title: "OFW ELITES 4 CWL LEADERBOARD",
-        updated: "UPDATED ON 6/2/25 : 9:30AM",
+        title: "OFW ELITES 4 JULY 2025 CWL",
+        updated: "MAINTAINED : MASTER LEAGUE I",
         players: [
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 }
+            { name: "R3N4N", stars: 21, percentage: 700 },
+            { name: "dgreat,one", stars: 21, percentage: 700 },
+            { name: "(12~jessie~27)", stars: 21, percentage: 700 },
+            { name: "♔ᶦᶰᵈ☤✞☤ᴀʟᴅᴡɪɴ♔", stars: 21, percentage: 700 },
+            { name: "⚡SHANKS⚡", stars: 20, percentage: 690 },
+            { name: "♥️KIDDO♥️", stars: 20, percentage: 690 },
+            { name: "HAMMER", stars: 20, percentage: 690 },
+            { name: "QF, JHAY", stars: 19, percentage: 692 },
+            { name: "BasePunisher(Sa", stars: 19, percentage: 690 },
+            { name: "MaCoH27", stars: 19, percentage: 689 },
         ]
     },
     {
-        title: "OFW ELITES 5 CWL LEADERBOARD",
-        updated: "UPDATED ON 6/3/25 : 11:00AM",
+        title: "IMMORTARS JULY 2025 CWL",
+        updated: "PROMOTED : MASTER LEAGUE III (LACK OF DATA)",
         players: [
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 },
-            { name: "NO DATA", stars: 0, percentage: 0 }
+            { name: "OE☆☆☆ReNaN☆☆☆", stars: 21, percentage: 700 },
+            { name: "D' BackupPlan", stars: 21, percentage: 700 },
+            { name: "Madisson", stars: 21, percentage: 700 },
+            { name: "bff ian", stars: 21, percentage: 700 },
+            { name: "JEFF DONATOR", stars: 21, percentage: 700 },
+            { name: "NO DATA", stars: 21, percentage: 700 },
+            { name: "NO DATA", stars: 21, percentage: 700 },
+            { name: "CHOOXtv", stars: 21, percentage: 700 },
+            { name: ">>♤♡MËL♢♧<<", stars: 20, percentage: 696 },
+            { name: "allison", stars: 20, percentage: 694 },
+        ]
+    },
+    {
+        title: "JUST 4 FUN JULY 2025 CWL",
+        updated: "PROMOTED : MASTER LEAGUE II",
+        players: [
+            { name: "Sharkie", stars: 21, percentage: 700 },
+            { name: "KIEFER", stars: 21, percentage: 700 },
+            { name: "Veldora", stars: 21, percentage: 700 },
+            { name: "OE⚔️Meliodas⚔️", stars: 21, percentage: 700 },
+            { name: "A B R A H A M", stars: 21, percentage: 700 },
+            { name: "NANATZY✔️", stars: 21, percentage: 700 },
+            { name: "JOHNJEZZPER", stars: 20, percentage: 692 },
+            { name: "RAYGUNNN", stars: 20, percentage: 690 },
+            { name: "NATSU dRAgoN", stars: 20, percentage: 684 },
+            { name: "PRINCE", stars: 19, percentage: 688 },
         ]
     }
 ];
